b748aba490d11d1dd22a73606840f12e8ff8b1cf
Telford Bett <telfordbett632@gmail.com>
2026-04-23 17:19:19 -0500

    Fix MongoDB URI with new password

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
-const MONGO_URI = "mongodb+srv://telfordbett632_db_user:<db_password>@cluster0.qcrjyfe.mongodb.net/sneakervault?retryWrites=true&w=majority";
+const MONGO_URI = "mongodb+srv://telfordbett632_db_user:Fm@DqC5JV7.EKEF@cluster0.qcrjyfe.mongodb.net/sneakervault?retryWrites=true&w=majority";
 const DB_NAME = "sneakervault";          // ← This is the database that will be created
 const JWT_SECRET = "sneakervault_super_secret_key_2026";
 
